52a269de6f8d29b0bc13c1cc465c0cbd89bbdeda
Jake Waksbaum <jake.waksbaum@gmail.com>
2015-11-17 21:28:06 -0500

    fix gui settings for mac

--- emacs.d/config/emacs-settings.el
+++ emacs.d/config/emacs-settings.el
@@ -13,18 +13,20 @@
 (setq inhibit-startup-message t)
 (fset 'yes-or-no-p 'y-or-n-p)
 (delete-selection-mode t)
-(scroll-bar-mode -1)
-(tool-bar-mode -1)
+(if (display-graphic-p)
+  (progn
+    (tool-bar-mode -1)
+    (scroll-bar-mode -1)
+    (set-fringe-style -1)
+    (tooltip-mode -1)))
 (blink-cursor-mode t)
 (show-paren-mode t)
 (column-number-mode t)
-(set-fringe-style -1)
-(tooltip-mode -1)
 
 ;; save history to .emacs.d
 (setq-default savehist-additional-variables
-	      '(search-ring regexp-search-ring)
-	      savehist-file (concat user-emacs-directory "savehist"))
+              '(search-ring regexp-search-ring)
+              savehist-file (concat user-emacs-directory "savehist"))
 (savehist-mode t)
 (setq make-backup-files nil)
 (setq auto-save-default nil)
