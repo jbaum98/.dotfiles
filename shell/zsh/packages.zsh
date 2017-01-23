d5e83db9d896485e7622732a959c329fd6486399
Jake Waksbaum <jake.waksbaum@gmail.com>
2017-01-23 17:21:20 -0500

    Switch to zplug

--- shell/zsh/packages.zsh
+++ shell/zsh/packages.zsh
@@ -1,22 +1,49 @@
-source "${DOTFILES}/shell/zsh/zgen/zgen.zsh"
+source "${DOTFILES}/shell/zsh/zplug/init.zsh"
 
-if ! zgen saved; then
-    echo "Creating a zgen save"
+zplug "zsh-users/zsh-history-substring-search"
+zplug "zsh-users/zsh-completions"
+zplug "zsh-users/zsh-autosuggestions"
 
-    zgen loadall <<EOPLUGINS
-        robbyrussell/oh-my-zsh lib/
-        zsh-users/zsh-syntax-highlighting
-        zsh-users/zsh-history-substring-search
-        zsh-users/zsh-completions
-        zsh-users/zsh-autosuggestions
-        srijanshetty/zsh-pandoc-completion
-        robbyrussell/oh-my-zsh plugins/git
-        robbyrussell/oh-my-zsh plugins/ssh-agent
-        robbyrussell/oh-my-zsh plugins/rake
-        robbyrussell/oh-my-zsh plugins/rbenv
-        robbyrussell/oh-my-zsh plugins/common-aliases
-        nojhan/liquidprompt
-EOPLUGINS
+zplug "srijanshetty/zsh-pandoc-completion"
 
-    zgen save
+zplug "plugins/git", from:oh-my-zsh
+zplug "plugins/ssh-agent", from:oh-my-zsh
+zplug "plugins/rake", from:oh-my-zsh
+zplug "plugins/rbenv", from:oh-my-zsh
+zplug "plugins/common-aliases", from:oh-my-zsh
+
+zplug "lib/clipboard", from:oh-my-zsh, if:"[[ $OSTYPE == *darwin* ]]"
+zplug "lib/theme-and-appearance", from:oh-my-zsh
+
+zplug "zsh-users/zsh-syntax-highlighting", defer:3
+
+zplug "nojhan/liquidprompt"
+
+# Install plugins if there are plugins that have not been installed
+if ! zplug check --verbose; then
+    printf "Install? [y/N]: "
+    if read -q; then
+        echo; zplug install
+    fi
 fi
+
+#setopt xtrace
+# Then, source plugins and add commands to $PATH
+zplug load
+#unsetopt xtrace
+
+#if ! zgen saved; then
+#    echo "Creating a zgen save"
+#
+#    zgen loadall <<EOPLUGINS
+#        robbyrussell/oh-my-zsh lib/
+#        zsh-users/zsh-syntax-highlighting
+#        zsh-users/zsh-history-substring-search
+#        zsh-users/zsh-completions
+#        zsh-users/zsh-autosuggestions
+#        srijanshetty/zsh-pandoc-completion
+#        nojhan/liquidprompt
+#EOPLUGINS
+#
+#    zgen save
+#fi
--- zprofile
+++ zprofile
@@ -2,4 +2,4 @@
 source ~/.profile
 
 # Load .zshrc, containing non-login related bash initializations.
-source ~/.zshrc
+# source ~/.zshrc
