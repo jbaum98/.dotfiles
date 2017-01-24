15a832fa41451bc7842c3efedac855fa2e744cfe
Jake Waksbaum <jake.waksbaum@gmail.com>
2017-01-23 20:38:55 -0500

    Add nox, rustfmt, racer

--- config.nix
+++ config.nix
@@ -8,10 +8,13 @@
     all = buildEnv {
       name = "all";
       paths = [
+        nox
         (if stdenv.isDarwin then "emacs-25.1-mac-6.0" else emacs)
         neovim
         vim
         rustc
+        rustfmt
+        rustracer
         go
         python
         python3
