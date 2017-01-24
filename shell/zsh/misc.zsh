3cd7e71ceb19d337ae4586e889fda885e94b722e
Jake Waksbaum <jake.waksbaum@gmail.com>
2017-01-23 19:38:41 -0500

    Add keybindings for movement

--- shell/zsh/misc.zsh
+++ shell/zsh/misc.zsh
@@ -20,6 +20,10 @@ autoload -U promptinit
 promptinit
 setopt prompt_subst
 
+bindkey '^[[1;3C' forward-word
+bindkey '^[[1;3D' backward-word
+bindkey '^A' beginning-of-line
+bindkey '^E' end-of-line
 bindkey '^[[1;5D' emacs-backward-word
 bindkey '^[[1;5C' emacs-forward-word
 bindkey '^[[A' history-substring-search-up
