2a50095f681f6b78909d8c483e38151fd852c79e
Jake Waksbaum <jake.waksbaum@gmail.com>
2018-03-23 15:35:32 -0400

    Add direnv

--- shell/.nixpkgs/env/shell-utils.nix
+++ shell/.nixpkgs/env/shell-utils.nix
@@ -1,8 +1,8 @@
-{ clang, curl, coreutils, diffutils, direnv, dos2unix, entr,
+{ clang, curl, coreutils, diffutils, dos2unix, entr,
   httpie, gnugrep, ripgrep, gawk, patch, gnutar, man, xz }:
 {
   env.default = [
-    clang curl coreutils diffutils direnv dos2unix entr
+    clang curl coreutils diffutils dos2unix entr
     httpie gnugrep ripgrep gawk patch gnutar man xz
   ];
 }
