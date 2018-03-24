c1311e32ebfbe297aba756ad852b71a361987ba1
Jake Waksbaum <jake.waksbaum@gmail.com>
2018-03-24 01:26:57 -0400

    shell: add wget

--- shell/.nixpkgs/env/shell-utils.nix
+++ shell/.nixpkgs/env/shell-utils.nix
@@ -1,8 +1,8 @@
 { clang, curl, coreutils, diffutils, dos2unix, entr,
-  httpie, gnugrep, ripgrep, gawk, patch, gnutar, man, xz }:
+  httpie, gnugrep, ripgrep, gawk, patch, gnutar, man, wget, xz }:
 {
   env.default = [
     clang curl coreutils diffutils dos2unix entr
-    httpie gnugrep ripgrep gawk patch gnutar man xz
+    httpie gnugrep ripgrep gawk patch gnutar man wget xz
   ];
 }
