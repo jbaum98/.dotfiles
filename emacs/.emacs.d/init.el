4552f575cc72f3d8e97aab3bb72bda1450401bbf
Jake Waksbaum <jake.waksbaum@gmail.com>
2018-03-25 04:04:24 -0400

    emacs: byte compile for faster init

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,7 +1,20 @@
+;;; init.el --- Emacs Initialization File
+;;
+;; Copyright (c) 2018 Jake Waksbaum
+;;
+;; Author: Jake Waksbaum <jake.waksbaum@gmail.com>
+;; URL: https://github.com/jbaum98/.dotfiles
+;;
+;; This file is not part of GNU Emacs.
+
+
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
 ;;(package-initialize)
-(require 'org)
-(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+
+(add-to-list 'load-path "~/.emacs.d/lisp")
+(setq load-prefer-newer t)
+(require 'config-lib)
+(jakemaks/ensure-config)
---
+++ emacs/.emacs.d/lisp/config-lib.el
@@ -0,0 +1,68 @@
+(defun jakemaks/tangle-section-canceled ()
+  "Check if the previous section header was CANC."
+  (save-excursion
+    (when (re-search-backward "^\\*+\\s-+\\(.*?\\)?\\s-*$" nil t)
+      (string-prefix-p "CANC" (match-string 1)))))
+
+(defun jakemaks/tangle-config-org (orgfile elfile)
+  "Tange source blocks from ORGFILE to ELFILE.
+
+Writes all source blocks that are not marked as =:tangle no=,
+have a source-code of =emacs-lisp=, and doesn't have the
+todo-marker CANC."
+  (let* (;; list where we cobble together body parts
+         (body-list ())
+         ;; disable special file handlers when loading .org files
+         (file-name-handler-alist nil)
+         ;; monster-regexp to extract pieces out of an .org file
+         (org-babel-src-block-regexp (concat
+                                      ;; (1) indentation                 (2) lang
+                                      "^\\([ \t]*\\)#\\+begin_src[ \t]+\\([^ \f\t\n\r\v]+\\)[ \t]*"
+                                      ;; (3) switches
+                                      "\\([^\":\n]*\"[^\"\n*]*\"[^\":\n]*\\|[^\":\n]*\\)"
+                                      ;; (4) header arguments
+                                      "\\([^\n]*\\)\n"
+                                      ;; (5) body
+                                      "\\([^\000]*?\n\\)??[ \t]*#\\+end_src")))
+    (with-temp-buffer
+      (insert-file-contents orgfile)
+      (goto-char (point-min))
+      (while (re-search-forward org-babel-src-block-regexp nil t)
+        (let ((lang (match-string 2))
+              (args (match-string 4))
+              (body (match-string 5))
+              (canc (jakemaks/tangle-section-canceled)))
+          (when (and (string= lang "emacs-lisp")
+                     (not (string-match-p ":tangle\\s-+no" args))
+                     (not canc))
+            (add-to-list 'body-list body)))))
+    (with-temp-file elfile
+      (apply 'insert (reverse body-list)))))
+
+(defun jakemaks/ensure-config ()
+  "Tangle configuration if necessary."
+  (let* ((config-org "~/.emacs.d/lisp/config.org")
+         (config-el (concat (file-name-sans-extension config-org) ".el"))
+         (config-elc (concat (file-name-sans-extension config-org) ".elc")))
+    (if (file-newer-than-file-p config-org config-elc)
+        (progn
+          (when (file-exists-p config-elc) (delete-file config-elc))
+          (when (file-newer-than-file-p config-org config-el)
+            (jakemaks/tangle-config-org config-org config-el))
+          (require 'config)
+          (let ((byte-compile-warnings '(not noruntime)))
+            (byte-compile-file config-el)))
+      (require 'config))))
+
+(defun jakemaks/compile-config ()
+  "Tangle and compile configuration if necessary."
+  (let* ((config-org "~/.emacs.d/lisp/config.org")
+         (config-el (concat (file-name-sans-extension config-org) ".el"))
+         (config-elc (concat (file-name-sans-extension config-org) ".elc")))
+    (when (file-newer-than-file-p config-org config-elc)
+      (delete-file config-elc)
+      (jakemaks/tangle-config-org config-org config-el)
+      (byte-compile-file config-el)
+      (delete-file config-el))))
+
+(provide 'config-lib)
